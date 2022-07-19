2519a64dc9ee8e61d5293fdd4098a165819e1ee3
LloydJr <98184266+LloydJr@users.noreply.github.com>
2022-07-19 09:49:35 -0400

    Stuck on a typo but completed lab

--- src/main/java/rocks/zipcodewilmington/tictactoe/Board.java
+++ src/main/java/rocks/zipcodewilmington/tictactoe/Board.java
@@ -8,31 +8,53 @@ import java.util.List;
  */
 public class Board {
     Character[][] matrix;
+
     public Board(Character[][] matrix) {
         this.matrix = matrix;
     }
 
     public Boolean isInFavorOfX() {
-        return null;
+//        return null;
+        if (getWinner().equals("X"))
+            return true;
+        return false;
     }
 
     public Boolean isInFavorOfO() {
-        return null;
+//        return null;
+        if (getWinner().equals("O"))
+            return true;
+        return false;
     }
 
     public Boolean isTie() {
-        return null;
+//        return null;
+        if (getWinner().equals(""))
+            return true;
+        return false;
     }
 
     public String getWinner() {
 //        return null;
         List<String> list = new ArrayList<String>();
+
         list.add(Character.toString(matrix[0][0]) + (matrix[0][1]) + (matrix[0][2]));
         list.add(Character.toString(matrix[1][0]) + (matrix[1][1]) + (matrix[1][2]));
         list.add(Character.toString(matrix[2][0]) + (matrix[2][1]) + (matrix[2][2]));
 
+        list.add(Character.toString(matrix[0][0]) + (matrix[1][0]) + (matrix[2][0]));
+        list.add(Character.toString(matrix[0][1]) + (matrix[1][1]) + (matrix[2][1]));
+        list.add(Character.toString(matrix[0][2]) + (matrix[1][2]) + (matrix[2][2]));
 
+        list.add(Character.toString(matrix[0][0]) + (matrix[1][1]) + (matrix[2][2]));
+        list.add(Character.toString(matrix[2][0]) + (matrix[1][1]) + (matrix[0][2]));
 
+        if (list.contains("XXX"))
+            return "X";
+        else if (list.contains("OOO"))
+            return "O";
+        else
+            return "";
     }
 
 }
